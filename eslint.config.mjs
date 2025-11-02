9cffa2d9e5e1c8fbce16e2c0a14e5312f91db5a4
Ankit <kumar.ankit.101012@gmail.com>
2025-11-02 18:33:20 +0530

    vitals removed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import next from "eslint-config-next";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
+  ...next,
   ...nextTs,
   {
     rules: {
